aa455becf6c85d2d483a957d7ba2d712d66c4a3c
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-03 02:51:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |x|
-    x == valid_calls
+    x == "Earth!"
     binding.pry
   end
 end
