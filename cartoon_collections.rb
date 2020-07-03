a8b4cb892430541415fab2e7a0e39946941a3116
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-03 03:00:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,6 +30,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |element|
     element == valid_calls || element == nil
-    binding.pry
   end
 end
